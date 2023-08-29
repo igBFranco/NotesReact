b1e904831fa887a0b659329a8af2a3329135d450
Igor Bueno Franco <88065589+igBFranco@users.noreply.github.com>
2023-08-29 20:28:24 -0300

    Adding auth when toggle

--- pages/ConfigScreen.js
+++ pages/ConfigScreen.js
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Switch, Text, TouchableOpacity, Image } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage'; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 export default function ConfigScreen({ route, navigation }) {
-
-  const [isAuthenticationEnabled, setIsAuthenticationEnabled] = useState(false); 
-
-  const toggleAuthentication = () => {
-    setIsAuthenticationEnabled(!isAuthenticationEnabled);
-    saveAuthenticationSetting(!isAuthenticationEnabled);
-  };
+  const [isAuthenticationEnabled, setIsAuthenticationEnabled] = useState(false);
 
   useEffect(() => {
     loadAuthenticationSetting();
@@ -26,6 +21,22 @@ export default function ConfigScreen({ route, navigation }) {
     }
   };
 
+  const toggleAuthentication = async () => {
+    if (isAuthenticationEnabled) {
+      const authResult = await LocalAuthentication.authenticateAsync({
+        promptMessage: 'Authenticate to disable biometric lock',
+      });
+
+      if (authResult.success) {
+        setIsAuthenticationEnabled(!isAuthenticationEnabled);
+        saveAuthenticationSetting(!isAuthenticationEnabled);
+      }
+    } else {
+      setIsAuthenticationEnabled(!isAuthenticationEnabled);
+      saveAuthenticationSetting(!isAuthenticationEnabled);
+    }
+  };
+
   const saveAuthenticationSetting = async (value) => {
     try {
       await AsyncStorage.setItem('authenticationEnabled', JSON.stringify(value));
@@ -34,28 +45,28 @@ export default function ConfigScreen({ route, navigation }) {
     }
   };
 
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-          headerTitle: '', 
-          headerRight: () => (
-            <View>
-              <TouchableOpacity onPress={() => navigation.navigate('Info')}>
-                <Image source={require('../assets/person.text.rectangle.png')} style={{width: 30, height: 30, marginRight: 20}}/>
-              </TouchableOpacity>
-            </View>
-            
-          ),
-        });
-      }, [navigation]);
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: '',
+      headerRight: () => (
+        <View>
+          <TouchableOpacity onPress={() => navigation.navigate('Info')}>
+            <Image
+              source={require('../assets/person.text.rectangle.png')}
+              style={{ width: 30, height: 30, marginRight: 20 }}
+            />
+          </TouchableOpacity>
+        </View>
+      ),
+    });
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-          Configurações
-      </Text>
+      <Text style={styles.title}>Configurações</Text>
       <View style={styles.toggleOption}>
-          <Text>Bloqueio por Biometria</Text>
-          <Switch value={isAuthenticationEnabled} onValueChange={toggleAuthentication} />
+        <Text>Bloqueio por Biometria</Text>
+        <Switch value={isAuthenticationEnabled} onValueChange={toggleAuthentication} />
       </View>
     </View>
   );
@@ -79,5 +90,5 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     justifyContent: 'space-between',
     alignItems: 'center',
-  }
+  },
 });
